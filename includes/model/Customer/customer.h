15426410adbc47699ff53de5733564d7db7f0abc
bkisly <b.kisly@gmail.com>
2022-05-15 19:53:37 +0200

    Implemented customer class

--- includes/model/Customer/customer.h
+++ includes/model/Customer/customer.h
@@ -5,9 +5,37 @@
 #ifndef CAFE_CUSTOMER_H
 #define CAFE_CUSTOMER_H
 
+#include <memory>
+#include "../MenuItem/menuItem.h"
+
+enum class CustomerState
+{
+    Unassigned,
+    SitTaken,
+    ReadyToOrder,
+    Awaiting,
+    Eating,
+    ReadyToPay,
+};
 
 class Customer {
+private:
+    unsigned int id;
+    bool allowsOthers;
+    unique_ptr<MenuItem> preferredMenuItem;
 
+    CustomerState currentState;
+
+public:
+    Customer(unsigned int id, bool allowsOthers, unique_ptr<MenuItem> preferredMenuItem);
+
+    unsigned int GetId() const;
+    bool DoesAllowOthers() const;
+    const unique_ptr<MenuItem> &GetPreferredMenuItem();
+    CustomerState GetCurrentState() const;
+
+    void AdvanceState();
+    string ToString();
 };
 
 
--- src/Customer/customer.cpp
+++ src/Customer/customer.cpp
@@ -2,4 +2,70 @@
 // Created by bkisl on 15.05.2022.
 //
 
-#include "../../includes/model/Table/customer.h"
+#include "../../includes/model/Customer/customer.h"
+
+Customer::Customer(unsigned int id, bool allowsOthers, unique_ptr<MenuItem> preferredMenuItem) {
+    this->id = id;
+    this->allowsOthers = allowsOthers;
+    this->preferredMenuItem = move(preferredMenuItem);
+    currentState = CustomerState::Unassigned;
+}
+
+unsigned int Customer::GetId() const {
+    return id;
+}
+
+bool Customer::DoesAllowOthers() const {
+    return allowsOthers;
+}
+
+const unique_ptr<MenuItem> &Customer::GetPreferredMenuItem() {
+    return preferredMenuItem;
+}
+
+CustomerState Customer::GetCurrentState() const {
+    return currentState;
+}
+
+void Customer::AdvanceState() {
+    currentState = (CustomerState)(((int)currentState + 1) % 6);
+}
+
+string Customer::ToString() {
+    string result = "Customer nr " + to_string(id) + " ";
+
+    switch (currentState) {
+        case CustomerState::SitTaken:
+        {
+            result += "has taken the seat.";
+            break;
+        }
+        case CustomerState::ReadyToOrder:
+        {
+            result += "is ready to order.";
+            break;
+        }
+        case CustomerState::Awaiting:
+        {
+            result += "is awaiting for " + preferredMenuItem->ToString();
+            break;
+        }
+        case CustomerState::Eating:
+        {
+            result += "is eating/drinking " + preferredMenuItem->ToString();
+            break;
+        }
+        case CustomerState::ReadyToPay:
+        {
+            result += "is ready to pay.";
+            break;
+        }
+        default:
+        {
+            result += "is not assigned to any table.";
+            break;
+        }
+    }
+
+    return result;
+}
