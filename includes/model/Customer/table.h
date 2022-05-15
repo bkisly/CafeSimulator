a8d4ddaa9f1f8c528ab974cd159aad60124aa02e
bkisly <b.kisly@gmail.com>
2022-05-15 21:02:52 +0200

    Implemented table class

--- includes/model/Customer/table.h
+++ includes/model/Customer/table.h
@@ -5,9 +5,26 @@
 #ifndef CAFE_TABLE_H
 #define CAFE_TABLE_H
 
+#include <vector>
+#include "customer.h"
 
 class Table {
+private:
+    unsigned int id;
+    unsigned int capacity;
+    vector<Customer> customers;
 
+    void customersValidation(const vector<Customer> &customers);
+
+public:
+    Table(unsigned int id, unsigned int capacity);
+
+    unsigned int GetId() const;
+    unsigned int GetCapacity() const;
+    const vector<Customer> &GetCustomers() const;
+
+    bool TryAddCustomers(vector<Customer> customers);
+    string ToString();
 };
 
 
--- src/Customer/table.cpp
+++ src/Customer/table.cpp
@@ -2,4 +2,65 @@
 // Created by bkisl on 15.05.2022.
 //
 
-#include "../../includes/model/Table/table.h"
+#include "../../includes/model/Customer/table.h"
+
+void Table::customersValidation(const vector<Customer> &customers) {
+    map<unsigned int, int> idOccurrences;
+
+    if(customers.empty())
+        throw invalid_argument("Customers collection must not be empty.");
+
+    for(auto iter = customers.begin(); iter < customers.end(); iter++)
+    {
+        if(iter > customers.begin() && iter->DoesAllowOthers() != (iter - 1)->DoesAllowOthers())
+            throw invalid_argument("All customers must have identical preference regarding other customers presence.");
+
+        unsigned int customerId = iter->GetId();
+
+        if(!idOccurrences.contains(customerId))
+            idOccurrences.insert(pair{ customerId, 0 });
+
+        idOccurrences[customerId]++;
+    }
+
+    for(const auto& [key, value] : idOccurrences)
+    {
+        if(value > 1) throw invalid_argument("Customer IDs must be unique.");
+    }
+}
+
+Table::Table(unsigned int id, unsigned int capacity) {
+    if(capacity == 0) throw invalid_argument("Table capacity must be greater than 0.");
+
+    this->id = id;
+    this->capacity = capacity;
+}
+
+unsigned int Table::GetId() const {
+    return id;
+}
+
+unsigned int Table::GetCapacity() const {
+    return capacity;
+}
+
+const vector<Customer> &Table::GetCustomers() const {
+    return customers;
+}
+
+bool Table::TryAddCustomers(vector<Customer> customers) {
+    if(customers.size() <= capacity - this->customers.size())
+    {
+        customersValidation(customers);
+        for(int i = 0; i < customers.size(); i++)
+            this->customers.push_back(customers[i]);
+
+        return true;
+    }
+
+    return false;
+}
+
+string Table::ToString() {
+    return "Table nr " + to_string(id);
+}
