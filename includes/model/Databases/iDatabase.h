cb043725e9330a173565388c6e93ecd03aa593a1
Wojtek Zarzecki <wojtek.zarzecki2002@gmail.com>
2022-06-01 10:52:36 +0200

    template database scheme

--- includes/model/Databases/iDatabase.h
+++ includes/model/Databases/iDatabase.h
@@ -19,4 +19,20 @@ public:
     virtual std::istream &Read(std::istream &is) = 0;
 };
 
+
+template<class T>
+class IDatabaseDebug {
+protected:
+    virtual void validateId(std::shared_ptr<T> newItem)=0;
+public:
+    virtual void foo()=0;
+    virtual void AddItem(std::shared_ptr<T> item) = 0;
+    virtual int CountItems()=0;
+    virtual void RemoveItem(unsigned int id) = 0;
+    virtual std::vector<std::shared_ptr<T>> &GetItems() = 0;
+
+    virtual std::ostream &Write(std::ostream &os) = 0;
+    virtual std::istream &Read(std::istream &is) = 0;
+};
+
 #endif //CAFE_IDATABASE_H
